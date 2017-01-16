86ef0f3341958d651b63485896d5259a59d99687
Muthama-Kahohi <paul.kahohi@andela.com>
2017-01-16 18:03:58 +0300

    Adds the config, run and requirements files

---
+++ config.py
@@ -0,0 +1,9 @@
+
+# Turn Debugging on
+DEBUG = True
+
+#secret key used in case of cookies
+SECRET_KEY = 'YouW1llN3v3erKn0w'
+
+#set the database url
+DATABASE_URI = 'sqlite:////temp/temp/bucketlist'
