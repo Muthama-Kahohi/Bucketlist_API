934e21931ca4f88934c495a3c729a7acd6f528c0
Muthama-Kahohi <paul.kahohi@andela.com>
2017-01-20 15:58:24 +0300

    Addds login and register code logic

--- bucketlist/app.py
+++ bucketlist/app.py
@@ -1,20 +1,54 @@
 from flask import Flask
-from flask_restful import Api, Resource
-
-app = Flask(__name__)
+from flask_restful import Api, Resource, reqparse
+from models import User
+from __init__ import app
 api = Api(app)
 
 
 class RegisterApi(Resource):
 
-    def post(self, username, password):
-        pass
+    def post(self):
+        '''Receives credentials and registers a new user using
+           using a username and a password
+        '''
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', required=True,
+                            help="Username cannot be blank")
+        parser.add_argument('password', required=True,
+                            help="Password cannot be blank")
+        credentials = parser.parse_args()
+        uname = credentials['username']
+        pword = credentials['password']
+        user = User(username=uname, password=pword)
+        # Hashes the password
+        user.hash_password(pword)
+        # Adds to the database
+        user.add(user)
+        return ({'message': 'user created'}, 201)
 
 
 class LoginApi(Resource):
 
-    def post(self, username, password):
-        pass
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', required=True,
+                            help="Username cannot be blank")
+        parser.add_argument('password', required=True,
+                            help="Password cannot be blank")
+        credentials = parser.parse_args()
+        uname = credentials['username']
+        pword = credentials['password']
+
+        # confirm that the user does exist
+        user = User.query.filter_by(username=uname).first()
+        if user is None:
+            return({'message': 'user does not exist'})
+        else:
+            # verify the password
+            if user.veriry_password(pword, user.password):
+                return({'message': 'Login successful'}, 200)
+            else:
+                return({'message': 'wrong password', 'passcode':user.password}, 400)
 
 
 class BucketLists(Resource):
@@ -49,18 +83,20 @@ class BucketListItem(Resource):
         pass
 
 
-api.add_resource(RegisterApi, 'bucketlist/api/v1.0/auth/register',
-                 endpoint='auth')
+api.add_resource(RegisterApi, '/bucketlist/api/auth/register')
 
-api.add_resource(LoginApi, 'bucketlist/api/v1.0/auth/login', endpoint='auth')
+api.add_resource(LoginApi, '/bucketlist/api/auth/login', )
 
-api.add_resource(BucketLists, 'bucketlist/api/v1.0/bucketlists',
-                 endpoint='bucketlists')
+api.add_resource(BucketLists, '/bucketlist/api/bucketlists',)
 api.add_resource(
-    BucketList, 'bucketlist/api/v1.0/bucketlists/<int:id>', endpoint='bucketlists')
+    BucketList, '/bucketlist/api/bucketlists/<int:id>', )
 
-app.add_resource(
-    BucketListItems, 'bucketlist/api/v1.0/bucketlists/<int:id>/items', endpoint='items')
+api.add_resource(
+    BucketListItems, '/bucketlist/api/bucketlists/<int:id>/items', )
 
-app.add_resource(
-    BucketListItem, 'bucketlist/api/v1.0/bucketlists/<int:id>/items/<int:item_id>', endpoint='items')
+api.add_resource(
+    BucketListItem, '/bucketlist/api/bucketlists/<int:id>/items/<int:item_id>', )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
