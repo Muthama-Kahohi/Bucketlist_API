19db0872349e665d98084b7cb50d2816a7c4c8e4
Muthama-Kahohi <paul.kahohi@andela.com>
2017-01-30 14:31:48 +0300

    Adds Blueprint and adds code to ensure no adding of blank registration

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -1,8 +1,21 @@
-from flask import Flask
-from . import config
-from instance import config
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from Config.config import configuration
+from flask_restful import Api
 
-app = Flask(__name__)
+api_blue_print = Blueprint('api', __name__, url_prefix='/bucketlist/api')
+# initialize the api class
+api = Api(api_blue_print)
 
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(configuration[config_name])
+    configuration[config_name].init_app(app)
+    db.init_app(app)
+
+    app.register_blueprint(api_blue_print)
+
+    return app
--- bucketlist/app.py
+++ bucketlist/app.py
@@ -1,8 +1,20 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-from models import User
-from __init__ import app
-api = Api(app)
+from flask_restful import Resource, reqparse
+from flask_httpauth import HTTPBasicAuth
+from bucketlist.models import User, BucketList
+from sqlalchemy import exc
+from flask import g
+
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(token):
+    # Authenticating the token
+    user = User.verify_token(token)
+    if not user:
+        return False
+    g.user = user
+    return True
 
 
 class RegisterApi(Resource):
@@ -11,20 +23,29 @@ class RegisterApi(Resource):
         '''Receives credentials and registers a new user using
            using a username and a password
         '''
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', required=True,
-                            help="Username cannot be blank")
-        parser.add_argument('password', required=True,
-                            help="Password cannot be blank")
-        credentials = parser.parse_args()
-        uname = credentials['username']
-        pword = credentials['password']
-        user = User(username=uname, password=pword)
-        # Hashes the password
-        user.hash_password(pword)
-        # Adds to the database
-        user.add(user)
-        return ({'message': 'user created'}, 201)
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('username', required=True,
+                                help="Username cannot be blank")
+            parser.add_argument('password', required=True,
+                                help="Password cannot be blank")
+            credentials = parser.parse_args()
+            uname = credentials['username']
+            pword = credentials['password']
+            # Ensure that registration details are not blank
+            if len(uname) <= 0 or len(pword) <= 0:
+                return({'message': 'please provide all credentials'}, 400)
+            user = User(username=uname, password=pword)
+            # Hashes the password
+            user.hash_password(pword)
+            # Adds to the database
+            user.add(user)
+            return ({'message': 'user created'}, 201)
+        except exc.IntegrityError:
+            return ({'message': 'user alredy exists'}, 400)
+
+    def get(self):
+        return({'message': 'Method is not allowed'}, 405)
 
 
 class LoginApi(Resource):
@@ -46,57 +67,53 @@ class LoginApi(Resource):
         else:
             # verify the password
             if user.veriry_password(pword, user.password):
-                return({'message': 'Login successful'}, 200)
+                token = user.generate_token()
+                return({'message': 'Login successful', 'token': token.decode('ascii')}, 200)
             else:
-                return({'message': 'wrong password', 'passcode':user.password}, 400)
+                return({'message': 'wrong username/password combination'}, 401)
+
+    def get(self):
+        return({'message': 'Method is not allowed'}, 405)
 
 
 class BucketLists(Resource):
+    @auth.login_required
     def get(self):
         pass
 
+    @auth.login_required
     def post(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('Bucketname', required=True,
+                            help="Bucket name cannot be blank")
+        parser.add_argument('')
 
 
 class BucketList(Resource):
+    @auth.login_required
     def get(self, id):
         pass
 
+    @auth.login_required
     def post(self, id):
         pass
 
+    @auth.login_required
     def delete(self, id):
         pass
 
 
 class BucketListItems(Resource):
+    @auth.login_required
     def post(self, id):
         pass
 
 
 class BucketListItem(Resource):
+    @auth.login_required
     def put(self, id):
         pass
 
+    @auth.login_required
     def delete(self, id):
         pass
-
-
-api.add_resource(RegisterApi, '/bucketlist/api/auth/register')
-
-api.add_resource(LoginApi, '/bucketlist/api/auth/login', )
-
-api.add_resource(BucketLists, '/bucketlist/api/bucketlists',)
-api.add_resource(
-    BucketList, '/bucketlist/api/bucketlists/<int:id>', )
-
-api.add_resource(
-    BucketListItems, '/bucketlist/api/bucketlists/<int:id>/items', )
-
-api.add_resource(
-    BucketListItem, '/bucketlist/api/bucketlists/<int:id>/items/<int:item_id>', )
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
